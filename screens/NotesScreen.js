ebc9eea70bdd8daf7ea1890f84cc56c99e00bfdb
m0n5terG <mikotai@yahoo.com>
2021-04-24 17:13:47 +0800

    new

--- screens/NotesScreen.js
+++ screens/NotesScreen.js
@@ -1,66 +1,95 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+ StyleSheet,
+ Text,
+ View,
+ FlatList,
+ TouchableOpacity,
+} from "react-native";
 import { Entypo } from "@expo/vector-icons";
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import * as SQLite from "expo-sqlite";
 
-export default function NotesScreen({ navigation }) {
-    const [notes, setNotes] = useState([
-      { title: "Walk the dog", done: false, id: "0" },
-      { title: "Feed the cat", done: false, id: "1" },
-      { title: "Bath the tiger", done: false, id: "2" },
-    ]);
-   
-    useEffect(() => {
-      navigation.setOptions({
-        headerRight: () => (
-          <TouchableOpacity onPress={addNote}>
-            <Entypo
-              name="new-message"
-              size={24}
-              color="black"
-              style={{ marginRight: 20 }}
-            />
-          </TouchableOpacity>
-        ),
-      });
-    });
-   
-    function addNote() {
-      navigation.navigate("Add Note");
-    }
-   
-    function renderItem({ item }) {
-      return (
-        <View
-          style={{
-            padding: 10,
-            paddingTop: 20,
-            paddingBottom: 20,
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-          }}
-        >
-          <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
-        </View>
-      );
-    }
-   
-    return (
-      <View style={styles.container}>
-        <FlatList
-          style={{ width: "100%" }}
-          data={notes}
-          renderItem={renderItem}
-        />
-      </View>
-    );
-   }
+const db = SQLite.openDatabase("notes.db");
 
-   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#e6ebff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
+export default function NotesScreen({ navigation, route }) {
+ const [notes, setNotes] = useState([
+   { title: "Walk the cat", done: false, id: "0" },
+   { title: "Feed the elephant", done: false, id: "1" },
+ ]);
+
+ useEffect(() => {
+   navigation.setOptions({
+     headerRight: () => (
+       <TouchableOpacity onPress={addNote}>
+         <Entypo
+           name="new-message"
+           size={24}
+           color="black"
+           style={{ marginRight: 20 }}
+         />
+       </TouchableOpacity>
+     ),
    });
+ });
+
+ useEffect(() => {
+    if (route.params?.text) {
+      db.transaction((tx) => {
+        tx.executeSql("INSERT INTO notes (done, value) VALUES (0, ?)", [
+          route.params.text,
+        ]);
+      });
+ 
+      const newNote = {
+        title: route.params.text,
+        done: false,
+        id: notes.length.toString(),
+      };
+      setNotes([...notes, newNote]);
+    }
+  }, [route.params?.text]);
+
+ function addNote() {
+   navigation.navigate("Add Note");
+ }
+
+ function renderItem({ item }) {
+   return (
+     <View
+       style={{
+         padding: 10,
+         paddingTop: 20,
+         paddingBottom: 20,
+         borderBottomColor: "#ccc",
+         borderBottomWidth: 1,
+       }}
+     >
+       <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+     </View>
+   );
+ }
+
+ return (
+   <View style={styles.container}>
+     <FlatList
+       style={{ width: "100%" }}
+       data={notes}
+       renderItem={renderItem}
+     />
+   </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: "#e6ebff",
+   alignItems: "center",
+   justifyContent: "center",
+ },
+});
+
+
+
+
+
