47efa09ff9e4199cd8feeab2b00e5c2a4191506f
m0n5terG <mikotai@yahoo.com>
2021-04-24 17:13:33 +0800

    new

--- App.js
+++ App.js
@@ -3,16 +3,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import AddScreen from "./screens/AddScreen";
 import NotesStack from "./screens/NotesStack";
-import * as SQLite from "expo-sqlite";
-
-const db = SQLite.openDatabase("notes.db");
 
 const Stack = createStackNavigator();
 
 export default function App() {
  return (
    <NavigationContainer>
-     <Stack.Navigator mode="modal" headerMode="none">
+     <Stack.Navigator mode="modal" headerMode=  "none">
        <Stack.Screen
          name="Notes Stack"
          component={NotesStack}
