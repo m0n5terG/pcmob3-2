cbb5b6ef9a8ecd0b4f3200832e2a0fb148c02eb0
m0n5terG <mikotai@yahoo.com>
2021-04-24 11:13:00 +0800

    new

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useEffect }  from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { createStackNavigator} from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
+import * as SQLite from 'expo-sqlite';
+
+const notesdb = SQLite.openDatabase('db.db');
+
+function NotesScreen({ navigation }) {
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Entypo
+            name="new-message"
+            size={24}
+            color="black"
+            style={{ marginRight: 20 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  });
+
+  function addNote() {
+    console.log("Add note");
+  }
+
+ return <View style={styles.container}></View>;
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen
+         name="Notes"
+         component={NotesScreen}
+         options={{
+           headerTitle: "Notes App",
+           headerTitleStyle: {
+             fontWeight: "bold",
+             fontSize: 30,
+           },
+           headerStyle: {
+             height: 120,
+             backgroundColor: "yellow",
+             borderBottomColor: "#ccc",
+             borderBottomWidth: 1,
+           },
+         }}
+       />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#ffc",
+   alignItems: "center",
+   justifyContent: "center",
+ },
 });
+
+
